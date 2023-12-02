dc2816d68d224bc30deea1e9284133a438eb3ab2
Aaron Meyer <ameyer@ucla.edu>
2023-12-02 07:56:03 -0800

    Switch to CuPy

---
+++ makefile
@@ -0,0 +1,10 @@
+
+test:
+	poetry run pytest -s -x -v
+
+testprofile:
+	poetry run python3 -m cProfile -o profile -m pytest -s -x -v
+	gprof2dot -f pstats --node-thres=1.0 profile | dot -Tsvg -o profile.svg
+
+clean:
+	rm profile.svg profile
--- parafac2/parafac2.py
+++ parafac2/parafac2.py
@@ -1,27 +1,25 @@
 import os
 from copy import deepcopy
 from typing import Sequence
-import torch
 import numpy as np
+import cupy as cp
 from tqdm import tqdm
 import tensorly as tl
-from tensorly.cp_tensor import CPTensor
 from tensorly.cp_tensor import cp_flip_sign, cp_normalize
-from tensorly.tenalg.svd import truncated_svd, randomized_svd
+from tensorly.tenalg.svd import randomized_svd
 from tensorly.decomposition import parafac
 from scipy.optimize import linear_sum_assignment
 
 
 def standardize_pf2(
-    weights: np.ndarray, factors: list[np.ndarray], projections: list[np.ndarray]
+    factors: list[np.ndarray], projections: list[np.ndarray]
 ) -> tuple[np.ndarray, list[np.ndarray], list[np.ndarray]]:
     # Order components by condition variance
     gini = np.var(factors[0], axis=0) / np.mean(factors[0], axis=0)
     gini_idx = np.argsort(gini)
     factors = [f[:, gini_idx] for f in factors]
-    weights = weights[gini_idx]
 
-    weights, factors = cp_normalize(cp_flip_sign((weights, factors), mode=1))
+    weights, factors = cp_flip_sign(cp_normalize((None, factors)), mode=1)
 
     # Order eigen-cells to maximize the diagonal of B
     _, col_ind = linear_sum_assignment(np.abs(factors[1].T), maximize=True)
@@ -38,35 +36,51 @@ def standardize_pf2(
 
 def _cmf_reconstruction_error(matrices: Sequence, factors: list, norm_X_sq):
     A, B, C = factors
+    xp = cp.get_array_module(B)
 
     norm_sq_err = norm_X_sq
     CtC = C.T @ C
     projections = []
-    projected_X = []
+    projected_X = xp.empty((A.shape[0], B.shape[0], C.shape[0]))
 
     for i, mat in enumerate(matrices):
-        if isinstance(B, torch.Tensor):
-            mat_gpu = torch.tensor(mat).cuda().double()
-        else:
-            mat_gpu = mat
+        mat_gpu = xp.array(mat)
 
         lhs = B @ (A[i] * C).T
-        U, _, Vh = truncated_svd(mat_gpu @ lhs.T, A.shape[1])
+        U, _, Vh = xp.linalg.svd(mat_gpu @ lhs.T, full_matrices=False)
         proj = U @ Vh
 
         projections.append(proj)
-        projected_X.append(proj.T @ mat_gpu)
+        projected_X[i] = proj.T @ mat_gpu
 
         B_i = (proj @ B) * A[i]
 
         # trace of the multiplication products
-        norm_sq_err -= 2.0 * tl.trace(A[i][:, np.newaxis] * B.T @ projected_X[-1] @ C)
+        norm_sq_err -= 2.0 * xp.trace(A[i][:, np.newaxis] * B.T @ projected_X[i] @ C)
         norm_sq_err += ((B_i.T @ B_i) * CtC).sum()
 
     return norm_sq_err, projections, projected_X
 
 
-@torch.inference_mode()
+def parafac2_init(
+    X_in: Sequence,
+    rank: int,
+    random_state=None,
+):
+    rng = np.random.RandomState(random_state)
+
+    # Assemble covariance matrix rather than concatenation
+    # This saves memory and should be faster
+    covM = X_in[0].T @ X_in[0]
+    for i in range(1, len(X_in)):
+        covM += X_in[i].T @ X_in[i]
+
+    C = randomized_svd(cp.array(covM), rank, random_state=rng, n_iter=4)[0]
+
+    factors = [cp.ones((len(X_in), rank)), cp.eye(rank), C]
+    return factors
+
+
 def parafac2_nd(
     X_in: Sequence,
     rank: int,
@@ -75,7 +89,6 @@ def parafac2_nd(
     random_state=None,
 ) -> tuple[np.ndarray, list[np.ndarray], list[np.ndarray], float]:
     r"""The same interface as regular PARAFAC2."""
-    rng = np.random.RandomState(random_state)
 
     # Verbose if this is not an automated build
     verbose = "CI" not in os.environ
@@ -85,39 +98,15 @@ def parafac2_nd(
 
     norm_tensor = np.sum([np.linalg.norm(xx) ** 2 for xx in X_in])
 
-    # Checks size of each experiment is bigger than rank
-    for i in range(len(X_in)):
-        assert np.shape(X_in[i])[0] > rank
-
-    # Checks size of signal measured is bigger than rank
-    assert np.shape(X_in[0])[1] > rank
-
-    # Assemble covariance matrix rather than concatenation
-    # This saves memory and should be faster
-    covM = X_in[0].T @ X_in[0]
-    for i in range(1, len(X_in)):
-        covM += X_in[i].T @ X_in[i]
-
-    C = randomized_svd(covM, rank, random_state=rng, n_iter=4)[0]
-
-    tl.set_backend("pytorch")
-    CP = CPTensor(
-        (
-            None,
-            [
-                tl.ones((len(X_in), rank)).cuda().double(),
-                tl.eye(rank).cuda().double(),
-                torch.tensor(C).cuda().double(),
-            ],
-        )
-    )
+    tl.set_backend("cupy")
+    factors = parafac2_init(X_in, rank, random_state)
 
     errs = []
 
     tq = tqdm(range(n_iter_max), disable=(not verbose))
     for iter in tq:
         err, projections, projected_X = _cmf_reconstruction_error(
-            X_in, CP.factors, norm_tensor
+            X_in, factors, norm_tensor
         )
 
         # Initiate line search
@@ -125,13 +114,13 @@ def parafac2_nd(
             jump = iter ** (1.0 / acc_pow)
 
             # Estimate error with line search
-            CP_ls = deepcopy(CP)
-            CP_ls.factors = [
-                CP_old.factors[ii] + (CP.factors[ii] - CP_old.factors[ii]) * jump
+            factors_ls = deepcopy(factors)
+            factors_ls = [
+                factors_old[ii] + (factors[ii] - factors_old[ii]) * jump  # type: ignore
                 for ii in range(3)
             ]
             err_ls, projections_ls, projected_X_ls = _cmf_reconstruction_error(
-                X_in, CP_ls.factors, norm_tensor
+                X_in, factors_ls, norm_tensor
             )
 
             if err_ls < err:
@@ -139,7 +128,7 @@ def parafac2_nd(
                 err = err_ls
                 projections = projections_ls
                 projected_X = projected_X_ls
-                CP = CP_ls
+                factors = factors_ls
             else:
                 acc_fail += 1
 
@@ -150,20 +139,17 @@ def parafac2_nd(
                     if verbose:
                         print("Reducing acceleration.")
 
-        errs.append(tl.to_numpy((err / norm_tensor).cpu()))
+        errs.append(cp.asnumpy((err / norm_tensor)))
 
-        # Project tensor slices
-        projected_X = tl.stack(projected_X)
-
-        CP_old: CPTensor = deepcopy(CP)
-        CP = parafac(
+        factors_old = deepcopy(factors)
+        _, factors = parafac(
             projected_X,
             rank,
-            n_iter_max=10,
-            init=CP, # type: ignore
+            n_iter_max=3,
+            init=(None, factors),  # type: ignore
             tol=False,
             normalize_factors=False,
-            l2_reg=0.0001, # type: ignore
+            l2_reg=0.0001,  # type: ignore
         )
 
         if iter > 1:
@@ -176,8 +162,7 @@ def parafac2_nd(
     R2X = 1 - errs[-1]
     tl.set_backend("numpy")
 
-    CP.factors = [f.numpy(force=True) for f in CP.factors]
-    CP.weights = CP.weights.numpy(force=True)
-    projections = [p.numpy(force=True) for p in projections]
+    factors = [cp.asnumpy(f) for f in factors]
+    projections = [cp.asnumpy(p) for p in projections]
 
-    return *standardize_pf2(CP.weights, CP.factors, projections), R2X
+    return *standardize_pf2(factors, projections), R2X
