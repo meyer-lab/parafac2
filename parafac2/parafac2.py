f90af78451b63fc6ca125d1955fb9c6dd24c502f
Aaron Meyer <ameyer@ucla.edu>
2024-04-28 07:18:34 -0700

    Ruff format and add store_pf2

--- parafac2/parafac2.py
+++ parafac2/parafac2.py
@@ -17,6 +17,24 @@ from .utils import (
 )
 
 
+def store_pf2(
+    X: anndata.AnnData, parafac2_output: tuple[np.ndarray, list, list]
+) -> anndata.AnnData:
+    """Store the Pf2 results into the anndata object."""
+    sgIndex = X.obs["condition_unique_idxs"]
+
+    X.uns["Pf2_weights"] = parafac2_output[0]
+    X.uns["Pf2_A"], X.uns["Pf2_B"], X.varm["Pf2_C"] = parafac2_output[1]
+
+    X.obsm["projections"] = np.zeros((X.shape[0], len(X.uns["Pf2_weights"])))
+    for i, p in enumerate(parafac2_output[2]):
+        X.obsm["projections"][sgIndex == i, :] = p
+
+    X.obsm["weighted_projections"] = X.obsm["projections"] @ X.uns["Pf2_B"]
+
+    return X
+
+
 def parafac2_init(
     X_in: anndata.AnnData,
     rank: int,
@@ -68,9 +86,7 @@ def parafac2_nd(
         means = cp.zeros((1, factors[2].shape[0]))
 
     projections, projected_X = project_data(X_list, means, factors)
-    err = reconstruction_error(
-        factors, projections, projected_X, norm_tensor
-    )
+    err = reconstruction_error(factors, projections, projected_X, norm_tensor)
     errs = [err]
 
     tl.set_backend("cupy")
@@ -81,8 +97,7 @@ def parafac2_nd(
 
         # Estimate error with line search
         factors_ls = [
-            factors_old[ii] + (factors[ii] - factors_old[ii]) * jump
-            for ii in range(3)
+            factors_old[ii] + (factors[ii] - factors_old[ii]) * jump for ii in range(3)
         ]
 
         projections_ls, projected_X_ls = project_data(X_list, means, factors)
@@ -103,9 +118,7 @@ def parafac2_nd(
             beta_i = beta_i / eta
 
             projections, projected_X = project_data(X_list, means, factors)
-            err = reconstruction_error(
-                factors, projections, projected_X, norm_tensor
-            )
+            err = reconstruction_error(factors, projections, projected_X, norm_tensor)
 
         errs.append(err / norm_tensor)
 
